27b23ab8e9720abd3db9d1722246ad105e52a528
Joshua Ho <ho.joshua4@gmail.com>
2019-06-29 22:16:28 -0700

    Database connecting properly

--- routes/mongoRouter.js
+++ routes/mongoRouter.js
@@ -1,28 +1,54 @@
 let express = require('express');
 let router = express.Router();
-let mongo = require('mongodb');
 
 var fs = require('fs');
 const csv = require('csv-parser');
 
-//./public/data/pokemon.csv
 let pokeArray = [];
 
+//Stream .csv data
 fs.createReadStream('./public/data/pokemon.csv')
   .pipe( csv() )
   .on('data', (row) => {
-    console.log(row)
+    // console.log(row)
     pokeArray.push(row)
   })
   .on('end', () => {
     console.log('CSV File successfully processed');
   });
 
+//Access DB
+
+const dbUrl = "mongodb+srv://ho_admin:ww7zqlswbdHMiyg2@pokemondb-o61qa.mongodb.net/test?retryWrites=true&w=majority"
+var db;
+let mongo = require('mongodb');
+let MongoClient = mongo.MongoClient;
+const client = new MongoClient(dbUrl, { useNewUrlParser: true });
+
+//Connect to DB
+client.connect(err => {
+  if (err) console.log(err);
+  const db = client.db("pokemon");
+  console.log('connected to database')
+  // perform actions on the collection object
+  client.close();
+});
+  
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+
+//Routes (/mongo)
 router.get('/', (req, res, next) => {
 
+  let array = pokeArray.splice(0,151)
   //Print out some data from the CSV file here:
-  res.send( pokeArray.splice(0,151) );
-  next();  
+  res.json( array[3] );
   
 });
 
